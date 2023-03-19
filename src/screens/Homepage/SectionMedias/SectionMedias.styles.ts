f40015d5043ad031c688b0a055998f45a73c5143
Jonathas <jonathasarruda@outlook.com>
2023-03-19 17:33:32 -0300

    Responsividade Seção Medias.

--- src/screens/Homepage/SectionMedias/SectionMedias.styles.ts
+++ src/screens/Homepage/SectionMedias/SectionMedias.styles.ts
@@ -3,16 +3,47 @@ import ButtonPrimary from "../../../components/ButtonPrimary";
 import { MediaQueries } from "../../../foundation/media";
 import { BrandColors, NeutralColors } from "../../../foundation/tokens";
 
-export const Container = styled.div`
-  width: 1078px;
-  height: 400px;
+export const SectionMediasDesktop = styled.div`
+  display: none;
+
+  @media ${MediaQueries.Medium} {
+    display: block;
+  }
+`;
+
+export const SectionMediasMobile = styled.div`
+  display: block;
+
+  @media ${MediaQueries.Medium} {
+    display: none;
+  }
+`;
+
+export const ContainerImg = styled.div`
+  width: 100%;
   display: flex;
+  align-items: center;
+  background-color: ${BrandColors.colorBrand02};
+  padding-top: 40px;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: space-between;
 
   section {
+    height: 100%;
     display: flex;
     flex-direction: column;
-    gap: 64px;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    gap: 24px;
+    margin: 32px 0;
   }
   div {
     display: flex;
@@ -22,7 +53,8 @@ export const Container = styled.div`
 
   h3 {
     padding-top: 0px;
-    font-size: 1.125rem;
+    font-size: 0.75rem;
+    text-align: center;
     margin: 0;
     padding: none;
     font-family: "Proxima Nova Semibold";
@@ -31,9 +63,39 @@ export const Container = styled.div`
   h2 {
     text-align: left;
     margin: 0;
-    font-size: 3rem;
+    font-size: 1.5rem;
+    text-align: center;
+
     font-family: "Proxima Nova Semibold";
     color: ${NeutralColors.darkest};
+  }
+
+  @media ${MediaQueries.Medium} {
+    width: 1078px;
+    height: 400px;
+    flex-direction: row;
+
+    section {
+      align-items: flex-start;
+      text-align: left;
+      gap: 64px;
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+    }
+
+    h3 {
+      padding-top: 0px;
+      font-size: 1.125rem;
+      text-align: left;
+    }
+    h2 {
+      text-align: left;
+      font-size: 3rem;
+      text-align: left;
+    }
   }
 `;
 
@@ -43,8 +105,11 @@ export const SectionYoutube = styled.div`
   justify-content: end;
   align-items: center;
   width: 100%;
-  height: 528px;
+  height: 100%;
   background-color: ${NeutralColors.light};
+  @media ${MediaQueries.Medium} {
+    height: 528px;
+  }
 `;
 export const SectionSpotify = styled.div`
   display: flex;
@@ -52,8 +117,11 @@ export const SectionSpotify = styled.div`
   justify-content: end;
   align-items: center;
   width: 100%;
-  height: 528px;
+  height: 100%;
   background-color: ${BrandColors.colorBrand03};
+  @media ${MediaQueries.Medium} {
+    height: 528px;
+  }
 `;
 
 export const PlayButton = styled(ButtonPrimary)`
--- src/screens/Homepage/SectionMedias/SectionMedias.tsx
+++ src/screens/Homepage/SectionMedias/SectionMedias.tsx
@@ -1,6 +1,9 @@
 import { SocialInfo } from "../../../api/data";
 import {
   Container,
+  ContainerImg,
+  SectionMediasDesktop,
+  SectionMediasMobile,
   SectionSpotify,
   SectionYoutube,
 } from "./SectionMedias.styles";
@@ -13,46 +16,94 @@ interface Props {
 const SectionSpotifyPlaylist: React.FC<Props> = ({ social }) => {
   return (
     <>
-      <SectionYoutube>
-        <Container>
-          <section>
-            <div>
-              <h3>PREGAÇÕES</h3>
-              <h2>
-                Acesse as pregações <br /> realizadas em nossa igreja
-              </h2>
-            </div>
-            <ButtonSecondary>Acessar canal no Youtube</ButtonSecondary>
-          </section>
-          <img
-            src="/images/youtubechannel.png"
-            alt="Picture of the author"
-            width={343}
-            height={400}
-          />
-        </Container>
-      </SectionYoutube>
-      <SectionSpotify>
-        <Container>
-          <>
+      <SectionMediasDesktop>
+        <SectionYoutube>
+          <Container>
+            <section>
+              <div>
+                <h3>PREGAÇÕES</h3>
+                <h2>
+                  Acesse as pregações <br /> realizadas em nossa igreja
+                </h2>
+              </div>
+              <ButtonSecondary>Acessar canal no Youtube</ButtonSecondary>
+            </section>
             <img
-              src="/images/spotify-playlist.png"
+              src="/images/youtubechannel.png"
               alt="Picture of the author"
               width={343}
               height={400}
             />
-          </>
-          <section>
-            <div>
-              <h3>MÚSICAS</h3>
-              <h2>
-                Ouça no Spotify as músicas <br /> que cantamos nos cultos
-              </h2>
-            </div>
-            <ButtonSecondary>Acessar canal no Youtube</ButtonSecondary>
-          </section>
-        </Container>
-      </SectionSpotify>
+          </Container>
+        </SectionYoutube>
+        <SectionSpotify>
+          <Container>
+            <>
+              <img
+                src="/images/spotify-playlist.png"
+                alt="Picture of the author"
+                width={343}
+                height={400}
+              />
+            </>
+            <section>
+              <div>
+                <h3>MÚSICAS</h3>
+                <h2>
+                  Ouça no Spotify as músicas <br /> que cantamos nos cultos
+                </h2>
+              </div>
+              <ButtonSecondary>Acessar canal no Youtube</ButtonSecondary>
+            </section>
+          </Container>
+        </SectionSpotify>
+      </SectionMediasDesktop>
+
+      <SectionMediasMobile>
+        <SectionYoutube>
+          <Container>
+            <ContainerImg>
+              <img
+                src="/images/youtubechannel.png"
+                alt="Picture of the author"
+                width={343}
+                height={400}
+              />
+            </ContainerImg>
+            <section>
+              <div>
+                <h3>PREGAÇÕES</h3>
+                <h2>
+                  Acesse as pregações <br /> realizadas em nossa igreja
+                </h2>
+              </div>
+              <ButtonSecondary>Acessar canal no Youtube</ButtonSecondary>
+            </section>
+          </Container>
+        </SectionYoutube>
+        <SectionSpotify>
+          <Container>
+            <ContainerImg>
+              <img
+                src="/images/spotify-playlist.png"
+                alt="Picture of the author"
+                width={343}
+                height={400}
+              />
+            </ContainerImg>
+
+            <section>
+              <div>
+                <h3>MÚSICAS</h3>
+                <h2>
+                  Ouça no Spotify as músicas <br /> que cantamos nos cultos
+                </h2>
+              </div>
+              <ButtonSecondary>Acessar canal no Youtube</ButtonSecondary>
+            </section>
+          </Container>
+        </SectionSpotify>
+      </SectionMediasMobile>
     </>
   );
 };
