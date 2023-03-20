f5b4a1deba6489cffb8ac10399f4802c4572f431
Jonathas <jonathasarruda@outlook.com>
2023-03-20 00:24:28 -0300

    Responsividade Seção Nossos Pastores.

--- src/screens/Homepage/SectionOurPastors/SectionOurPastors.styles.ts
+++ src/screens/Homepage/SectionOurPastors/SectionOurPastors.styles.ts
@@ -2,10 +2,9 @@ import styled from "styled-components";
 import { MediaQueries } from "../../../foundation/media";
 import { BrandColors, NeutralColors } from "../../../foundation/tokens";
 
-export const SectionOurPastorsContainer = styled.div`
+export const SectionOurPastorsContainerMobile = styled.div`
   width: 100%;
-  padding-top: 128px;
-  padding-bottom: 160px;
+  padding: 80px 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -13,32 +12,91 @@ export const SectionOurPastorsContainer = styled.div`
   background-color: ${BrandColors.colorBrand03};
   gap: 60px;
 
+  @media ${MediaQueries.Medium} {
+    display: none;
+  }
+`;
+
+export const SectionOurPastorsContainerDesktop = styled.div`
+  display: none;
+
+  @media ${MediaQueries.Medium} {
+    width: 100%;
+    padding-top: 128px;
+    padding-bottom: 160px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: ${BrandColors.colorBrand03};
+    gap: 60px;
+
+    h4 {
+      padding-top: 0px;
+      font-size: 1.625rem;
+      margin: 0;
+
+      padding: none;
+      font-family: "Proxima Nova Semibold";
+      font-weight: 100;
+      color: ${NeutralColors.darkest};
+    }
+  }
+`;
+export const ContainerOurPastors = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   h4 {
     padding-top: 0px;
-    font-size: 1.625rem;
-    margin: 0;
-    
+    font-size: 1.125rem;
+    margin: 60px 0 40px 0;
+
     padding: none;
     font-family: "Proxima Nova Semibold";
     font-weight: 100;
     color: ${NeutralColors.darkest};
   }
+
+  @media ${MediaQueries.Medium} {
+    h4 {
+      padding-top: 0px;
+      font-size: 1.625rem;
+      margin: 80px 0 40px 0;
+
+      padding: none;
+      font-family: "Proxima Nova Semibold";
+      font-weight: 100;
+      color: ${NeutralColors.darkest};
+    }
+  }
+`;
+export const ExternalStylingProfileCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
 
 export const CardsContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: flex-start;
+  align-items: center;
   padding: 0px;
-  gap: 16px;
+  width: 100%;
 
-  width: 1250px;
+  @media ${MediaQueries.Medium} {
+    gap: 16px;
+}
 `;
 
 export const SectionTextOurPastors = styled.div`
-  width: 950px;
-  height: 155px;
+  width: 90%;
+  height: 100%;
   color: ${NeutralColors.darkest};
   display: flex;
   flex-direction: column;
@@ -46,19 +104,45 @@ export const SectionTextOurPastors = styled.div`
   align-items: center;
   gap: 24px;
 
-  h2 {
-    margin: 0;
-    font-size: 3rem;
-    text-align: center;
-    font-family: "Proxima Nova Semibold";
-  }
-
   h3 {
     padding-top: 0px;
-    font-size: 1.125rem;
+    font-size: 0.75rem;
     margin: 0;
     padding: none;
     font-family: "Proxima Nova Semibold";
     color: ${BrandColors.colorBrand02};
+  }
+  h2 {
+    margin: 0;
+    font-size: 1.5rem;
+    text-align: center;
+    font-family: "Proxima Nova Semibold";
+  }
+
+  @media ${MediaQueries.Medium} {
+    width: 950px;
+    height: 155px;
+    color: ${NeutralColors.darkest};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+
+    h2 {
+      margin: 0;
+      font-size: 3rem;
+      text-align: center;
+      font-family: "Proxima Nova Semibold";
+    }
+
+    h3 {
+      padding-top: 0px;
+      font-size: 1.125rem;
+      margin: 0;
+      padding: none;
+      font-family: "Proxima Nova Semibold";
+      color: ${BrandColors.colorBrand02};
+    }
   }
 `;
--- src/screens/Homepage/SectionOurPastors/SectionOurPastors.tsx
+++ src/screens/Homepage/SectionOurPastors/SectionOurPastors.tsx
@@ -1,9 +1,14 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { data } from "../../../api/data";
 import ProfileCard from "../../../components/ProfileCard";
 
 import {
   CardsContainer,
-  SectionOurPastorsContainer,
+  ContainerOurPastors,
+  ExternalStylingProfileCard,
+  SectionOurPastorsContainerDesktop,
+  SectionOurPastorsContainerMobile,
   SectionTextOurPastors,
 } from "./SectionOurPastors.styles";
 
@@ -12,44 +17,99 @@ const deacons = data.churchInfo.deacons;
 
 const SectionOurPastors = () => {
   return (
-    <SectionOurPastorsContainer id="atividades">
-      <SectionTextOurPastors>
-        <h3>EQUIPE PASTORAL</h3>
-        <h2>
-          Somos liderados por <br /> um corpo de presbíteros{" "}
-        </h2>
-      </SectionTextOurPastors>
-      <h4>Presbitério da Cidade Cinco</h4>
-      <CardsContainer>
-        {pastors.map((pastor, i) => {
-          return (
-            <ProfileCard
-              key={i}
-              id={i}
-              imageUrl={pastor.imageUrl}
-              name={pastor.fullName}
-              title={pastor.title}
-              description={pastor.description}
-            ></ProfileCard>
-          );
-        })}
-      </CardsContainer>
-      <h4>Diáconos da Cidade Cinco</h4>
-      <CardsContainer>
-        {deacons.map((deacon, i) => {
-          return (
-            <ProfileCard
-              key={i}
-              id={i}
-              imageUrl={deacon.imageUrl}
-              name={deacon.fullName}
-              title={deacon.title}
-              description={deacon.description}
-            ></ProfileCard>
-          );
-        })}
-      </CardsContainer>
-    </SectionOurPastorsContainer>
+    <>
+      <SectionOurPastorsContainerMobile id="atividades">
+        <ContainerOurPastors>
+          <SectionTextOurPastors>
+            <h3>EQUIPE PASTORAL</h3>
+            <h2>
+              Somos liderados por <br /> um corpo de presbíteros e diáconos.
+            </h2>
+          </SectionTextOurPastors>
+          <h4>Presbitério da Cidade Cinco</h4>
+          <CardsContainer>
+            <Swiper>
+              {pastors.map((pastor, i) => {
+                return (
+                  <SwiperSlide>
+                    <ExternalStylingProfileCard>
+                      <ProfileCard
+                        key={i}
+                        id={i}
+                        imageUrl={pastor.imageUrl}
+                        name={pastor.fullName}
+                        title={pastor.title}
+                        description={pastor.description}
+                      />
+                    </ExternalStylingProfileCard>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </CardsContainer>
+          <h4>Diáconos da Cidade Cinco</h4>
+          <CardsContainer>
+            <Swiper>
+              {deacons.map((deacon, i) => {
+                return (
+                  <SwiperSlide>
+                    <ExternalStylingProfileCard>
+                      <ProfileCard
+                        key={i}
+                        id={i}
+                        imageUrl={deacon.imageUrl}
+                        name={deacon.fullName}
+                        title={deacon.title}
+                        description={deacon.description}
+                      />
+                    </ExternalStylingProfileCard>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </CardsContainer>
+        </ContainerOurPastors>
+      </SectionOurPastorsContainerMobile>
+
+      <SectionOurPastorsContainerDesktop id="atividades">
+        <SectionTextOurPastors>
+          <h3>EQUIPE PASTORAL</h3>
+          <h2>
+            Somos liderados por <br /> um corpo de presbíteros e diáconos.
+          </h2>
+        </SectionTextOurPastors>
+        <h4>Presbitério da Cidade Cinco</h4>
+        <CardsContainer>
+          {pastors.map((pastor, i) => {
+            return (
+              <ProfileCard
+                key={i}
+                id={i}
+                imageUrl={pastor.imageUrl}
+                name={pastor.fullName}
+                title={pastor.title}
+                description={pastor.description}
+              ></ProfileCard>
+            );
+          })}
+        </CardsContainer>
+        <h4>Diáconos da Cidade Cinco</h4>
+        <CardsContainer>
+          {deacons.map((deacon, i) => {
+            return (
+              <ProfileCard
+                key={i}
+                id={i}
+                imageUrl={deacon.imageUrl}
+                name={deacon.fullName}
+                title={deacon.title}
+                description={deacon.description}
+              ></ProfileCard>
+            );
+          })}
+        </CardsContainer>
+      </SectionOurPastorsContainerDesktop>
+    </>
   );
 };
 
